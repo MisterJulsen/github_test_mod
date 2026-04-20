4325a88114d9e66edc86327eb63bf1feae1d51c0
MrJulsen <geisterdrache02@gmail.com>
2026-04-20 21:47:39 +0200

    Pain 9

--- common/src/main/java/de/mrjulsen/githubtest/GithubTestMod.java
+++ common/src/main/java/de/mrjulsen/githubtest/GithubTestMod.java
@@ -1,9 +1,16 @@
 package de.mrjulsen.githubtest;
 
+import dev.architectury.event.events.client.ClientPlayerEvent;
+import net.minecraft.network.chat.Component;
+
 public final class GithubTestMod {
     public static final String MOD_ID = "github_test_mod";
 
     public static void init() {
         System.out.println("Hello World!");
+
+        ClientPlayerEvent.CLIENT_PLAYER_JOIN.register(p -> {
+            p.sendSystemMessage(Component.translatable("item.testmod.dragon_scale"));
+        });
     }
 }
