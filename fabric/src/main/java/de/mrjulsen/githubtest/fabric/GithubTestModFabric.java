5bade6f69060f875e8017f77c952ebcaa2f42426
MrJulsen <geisterdrache02@gmail.com>
2026-04-20 20:35:11 +0200

    Pain

--- fabric/src/main/java/de/mrjulsen/githubtest/fabric/GithubTestModFabric.java
+++ fabric/src/main/java/de/mrjulsen/githubtest/fabric/GithubTestModFabric.java
@@ -1,6 +1,6 @@
 package de.mrjulsen.githubtest.fabric;
 
-import de.mrjulsen.githubtest.GithubTestTmod;
+import de.mrjulsen.githubtest.GithubTestMod;
 import net.fabricmc.api.ModInitializer;
 
 public final class GithubTestModFabric implements ModInitializer {
--- forge/src/main/java/de/mrjulsen/githubtest/forge/GithubTestModForge.java
+++ forge/src/main/java/de/mrjulsen/githubtest/forge/GithubTestModForge.java
@@ -5,10 +5,10 @@ import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(GithubTestTmod.MOD_ID)
-public final class GithubTestTmodForge {
-    public GithubTestTmodForge() {
-        EventBuses.registerModEventBus(GithubTestTmod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+@Mod(GithubTestMod.MOD_ID)
+public final class GithubTestModForge {
+    public GithubTestModForge() {
+        EventBuses.registerModEventBus(GithubTestMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         GithubTestMod.init();
     }
 }
